4355bd2f354d02916b4d10832dde3b10a9033046
Ali Zargar Shabestari <alizx.1370@gmail.com>
2020-08-25 17:28:23 +0430

    SMP support added

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftZSTD",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftZSTD",
+            targets: ["SwiftZSTD"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+
+        .target(
+            name: "zstd",
+            dependencies: [],
+            cSettings: [
+                .headerSearchPath("common"),
+            ]
+        ),
+
+        // .target(
+        //     name: "objc",
+        //     dependencies: ["zstd"]
+        // ),
+
+        .target(
+            name: "SwiftZSTD",
+            dependencies: ["zstd"]),
+
+    ]
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CzstdTests
+
+var tests = [XCTestCaseEntry]()
+tests += CzstdTests.allTests()
+XCTMain(tests)
